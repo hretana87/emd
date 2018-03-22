fb8034cdb93ea27b5f9bad431594cbbf893b0324
hretana87 <hretana87@gmail.com>
2018-03-21 23:10:08 -0600

    send email first try

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  resources :messages
+  root 'messages#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
