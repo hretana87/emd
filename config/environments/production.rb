20a318bcd732fe4e71ce2658e0d4dd07c0376bd7
hretana87 <hretana87@gmail.com>
2018-04-04 18:01:45 -0600

    asdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => ENV["GMAIL_USERNAME"],
- :password             => ENV["GMAIL_PASSWORD"],
+ :user_name            => "henryemd666@gmail.com",
+ :password             => "lamadreteresa",
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
